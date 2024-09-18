21ad6e3accff874b89bafd8e0a55639f1c0b59e3
Daizu <daizutabi@gmail.com>
2024-09-18 11:02:10 +0000

    namespace

--- tests/test_ext_sweeper_plugin.py
+++ tests/test_ext_sweeper_plugin.py
@@ -8,59 +8,56 @@ from hydra.test_utils.launcher_common_tests import (
 from hydra.test_utils.test_utils import TSweepRunner
 from pytest import mark
 
+from hydra_plugins.hydra_ext_sweeper.ext_sweeper import ExtSweeper
+
 
 def test_discovery() -> None:
-    import sys
-
-    print(sys.path)
-    # Tests that this plugin can be discovered via the plugins subsystem when looking at
-    # the Sweeper plugins
     plugin_names = [x.__name__ for x in Plugins.instance().discover(Sweeper)]
-    print(plugin_names)
-    assert 0
+    assert ExtSweeper.__name__ in plugin_names
 
 
-# def test_cmd_line_overrides_launch_jobs(hydra_sweep_runner: TSweepRunner) -> None:
-#     sweep = hydra_sweep_runner(
-#         calling_file=None,
-#         calling_module="hydra.test_utils.a_module",
-#         config_path="configs",
-#         config_name="compose.yaml",
-#         task_function=None,
-#         overrides=["hydra/sweeper=experiment", "hydra/launcher=basic", "foo=1,2"],
-#     )
-#     with sweep:
-#         assert sweep.returns is not None
-#         job_ret = sweep.returns[0]
-#         assert len(job_ret) == 2
-#         assert job_ret[0].overrides == ["foo=1"]
-#         assert job_ret[0].cfg == {"foo": 1, "bar": 100}
-#         assert job_ret[1].overrides == ["foo=2"]
-#         assert job_ret[1].cfg == {"foo": 2, "bar": 100}
+def test_cmd_line_overrides_launch_jobs(hydra_sweep_runner: TSweepRunner) -> None:
+    sweep = hydra_sweep_runner(
+        calling_file=None,
+        calling_module="hydra.test_utils.a_module",
+        config_path="configs",
+        config_name="compose.yaml",
+        task_function=None,
+        overrides=["hydra/sweeper=ext", "hydra/launcher=basic", "foo=1,2"],
+    )
+    with sweep:
+        assert sweep.returns is not None
+        job_ret = sweep.returns[0]
+        assert len(job_ret) == 2
+        assert job_ret[0].overrides == ["foo=1"]
+        assert job_ret[0].cfg == {"foo": 1, "bar": 100}
+        assert job_ret[1].overrides == ["foo=2"]
+        assert job_ret[1].cfg == {"foo": 2, "bar": 100}
 
 
-# def test_configured_overrides_launch_jobs(hydra_sweep_runner: TSweepRunner) -> None:
-#     sweep = hydra_sweep_runner(
-#         calling_file=__file__,
-#         calling_module=None,
-#         config_path="configs",
-#         config_name="overrides.yaml",
-#         task_function=None,
-#         overrides=None,
-#     )
-#     with sweep:
-#         job_ret = sweep.returns[0]
-#         all_cfgs = set([frozenset(j.cfg.items()) for j in job_ret])
+def test_configured_overrides_launch_jobs(hydra_sweep_runner: TSweepRunner) -> None:
+    sweep = hydra_sweep_runner(
+        calling_file=__file__,
+        calling_module=None,
+        config_path="configs",
+        config_name="overrides.yaml",
+        task_function=None,
+        overrides=None,
+    )
+    with sweep:
+        job_ret = sweep.returns[0]
+        all_cfgs = set([frozenset(j.cfg.items()) for j in job_ret])
 
-#         expected_cfgs = [
-#             {"foo": "a", "bar": 1},
-#             {"foo": 3, "bar": 1},
-#             {"foo": True, "bar": 1},
-#             {"foo": "a", "bar": True},
-#             {"foo": 3, "bar": True},
-#             {"foo": True, "bar": True},
-#         ]
-#         assert all_cfgs == set([frozenset(cfg.items()) for cfg in expected_cfgs])
+        expected_cfgs = [
+            {"foo": "a", "bar": 1},
+            {"foo": 3, "bar": 1},
+            {"foo": True, "bar": 1},
+            {"foo": "a", "bar": True},
+            {"foo": 3, "bar": True},
+            {"foo": True, "bar": True},
+        ]
+        assert all_cfgs == set([frozenset(cfg.items())
+                               for cfg in expected_cfgs])
 
 
 # def test_cmd_line_overrides_overwrite_configured_overrides(
